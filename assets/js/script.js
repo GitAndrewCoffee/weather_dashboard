4d95ce51818a15d4d1a8397b4c21b43cae3df297
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-11-15 18:26:44 -0600

    started pulling forcast from api object

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,21 +35,13 @@
 
         console.log("getMyWeather is running");
 
-        var myCityWeather = getCityWeather(myCity);
-        
-        console.log("myCityWeather is " + myCityWeather);
-
-        var myLocWeather = getLocWeather(myLat, myLon);
-
-        console.log(myLocWeather);
+        getCityWeather(myCity);
 
     };
 
     function getCityWeather (cityName) {
 
         console.log("getCityWeather is running");
-
-        var returnMe = "";
 
         var callMe = cityCallURL + "?q=" + cityName + "&appid=" + apiKey;
 
@@ -64,19 +56,11 @@
             
             console.log(data);
 
-            console.log(data.coord);
+            cityLat = data.coord.lat;
 
-            console.log(data.coord.lat);
+            cityLon = data.coord.lon;
 
-            myLat = data.coord.lat;
-
-            console.log(data.coord.lon);
-
-            myLon = data.coord.lon;
-
-            var tempData = getLocWeather(myLat, myLon);
-
-            console.log(tempData);
+            getLocWeather(cityLat, cityLon);
 
             buildWeather(data);
 
@@ -88,8 +72,8 @@
     function getLocWeather(myLat, myLon) {
 
         console.log("getLocWeather is running")
-
-        var callMe = oneCallURL + "?lat=" + myLat + "&lon=" + myLon + "&appid=" + apiKey;
+   
+        var callMe = oneCallURL + "lat=" + myLat + "&lon=" + myLon + "&appid=" + apiKey;
 
         console.log("callMe is " + callMe);
 
@@ -109,7 +93,14 @@
     function buildWeather(myWeather){
 
         console.log("buildWeather is running");
+
         console.log(myWeather);
+
+        $('#cityName').text(myWeather.name);
+
+        $('#tempLable').text(myWeather.main.temp);
+        $('#windLable').text(myWeather.wind.speed);
+        $('#humidityLable').text(myWeather.main.humidity);
 
     }
 
@@ -117,8 +108,54 @@
 
         console.log("buildForecast is running");
         console.log(myForecast);
-        
-    }
+        var fiveDayDate = [];
+
+
+        //Set the UV index label
+        $('#uvLable').text(myForecast.current.uvi);
+
+        //build a list of dates for the forecast
+        for (let i = 0; i < 5; i++) {
+         
+            fiveDayDate[i] = moment().add(i + 1, "days").format("MM/DD/YYYY");
+            console.log("moment " + i + " is " + fiveDayDate[i]);
+            
+        }
+       
+        //split forecast into days
+
+        for (let i = 1; i < 6; i++) {
+            
+             console.log("day " + i + " is " + myForecast.daily[i].temp.day);
+             console.log("day " + i + " is " + myForecast.daily[i].wind_speed);
+             console.log("day " + i + " is " + myForecast.daily[i].humidity);
+             console.log("day " + i + " is " + myForecast.daily[i].weather[0]);
+             console.log("date " + i + " is " + fiveDayDate[i-1]);
+
+            //build cards
+            //add cards to the forecast section
+
+            //forecastIcon = myForecast.daily[i].weather[0];
+              
+            console.log(buildCard(fiveDayDate[i-1], cardIcon, cardTemp, cardTemp, cardWind, cardHumidity));
+
+            //  $("#fiveDay").add(buildCard(fiveDayDate[i-1], cardIcon, cardTemp, cardTemp, cardWind, cardHumidity));
+            
+        };           
+    };
+
+    function buildCard(myDate,myIcon,myTemp,myWind,myHumidity) {
+
+        console.log("buildCard is running");
+        console.log(myDate);
+        console.log(myIcon);
+        console.log(myTemp);
+        console.log(myWind);
+        console.log(myHumidity);
+
+        return "help me, I'm broken inside";
+
+    };
 
     var myWeather = getMyWeather("Boston");
     
